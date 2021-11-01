ec2fe853c35fe98ce5260f84814b85c59135469d
karubina <karpova.ekaterina19@gmail.com>
2021-11-01 13:15:30 +0300

    auth commit

--- demoauth/ViewController.swift
+++ demoauth/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var inoutLogin: UITextField!
+    @IBOutlet weak var inputPassword: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func authing(_ sender: Any) {
+        
+        guard !inoutLogin.text!.isEmpty else {
+            return showAlertDialog(message: "Brat, you slep? Where login?")
+        }
+        guard !inputPassword.text!.isEmpty else {
+            return showAlertDialog(message: "Brat, you stupid? Where parol to?")
+        }
+        
+        performSegue(withIdentifier: "auth", sender: nil)
+    }
+    
+    
+    func showAlertDialog(message: String) {
+        let alert = UIAlertController(title: "Errrrrorrrrr", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Nu ok(", style: .default, handler:(nil)))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    
+    
+    
+    
 }
 
